6f0f89ba4af5b7170941f9cd784421a7e63ac8c1
Cooper Maa <coopermaa77@gmail.com>
2016-09-09 15:34:36 +0000

    Fix a typo in response status line

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ puts "Listening on port 8080..."
 loop do
   socket = server.accept
   request_line = socket.gets
-  socket.print "HTTP/1.1 200/OK\r\n"
+  socket.print "HTTP/1.1 200 OK\r\n"
   socket.print "Content-type: text/html\r\n"
   socket.print "\r\n"
   socket.print("Hello World")
