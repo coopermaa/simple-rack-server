aad4d82349afb385c5329b525d80f683b0495cfc
Cooper Maa <coopermaa77@gmail.com>
2016-09-10 00:31:51 +0000

    Add error handle code in server (encounter on Heroku)

--- server.rb
+++ server.rb
@@ -12,18 +12,25 @@ class Server
     puts "Listening on port #{ENV['PORT']  || 8080}..."
 
     loop do
-      socket   = @tcp_server.accept
-      request_line  = socket.gets
+      socket = nil
+      begin
+        socket   = @tcp_server.accept
+        request_line  = socket.gets
 
-      env = new_env(*request_line.split)
-      status, headers, body = @app.call(env)
+        env = new_env(*request_line.split)
+        status, headers, body = @app.call(env)
 
-      socket.print "HTTP/1.1 #{status} #{STATUS_CODES[status]}\r\n"
-      headers.each { |k, v| socket.print "#{k}: #{v}\r\n" }
-      socket.print "\r\n"
+        socket.print "HTTP/1.1 #{status} #{STATUS_CODES[status]}\r\n"
+        headers.each { |k, v| socket.print "#{k}: #{v}\r\n" }
+        socket.print "\r\n"
       
-      body.each { |chunk| socket.print chunk }
-      socket.close
+        body.each { |chunk| socket.print chunk }
+        socket.close
+      rescue Exception => e
+        puts "#{e.class}: #{e.message}"
+        socket.close
+        retry
+      end
     end
   end
 
